77de9c3f27eef864c31f189d2c5b0fa2600d856f
Sean Villars <sean@setpoint.io>
2024-08-14 15:00:44 -0500

    reformat lua files

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,5 +1,5 @@
 -- Pull in the wezterm API
-local wezterm = require("wezterm")
+local wezterm = require 'wezterm'
 local act = wezterm.action
 
 -- This will hold the configuration.
@@ -8,138 +8,152 @@ local config = wezterm.config_builder()
 -- This is where you actually apply your config choices
 
 -- For example, changing the color scheme:
-config.color_scheme = "Tokyo Night"
+config.color_scheme = 'Tokyo Night'
 
 config.window_background_opacity = 0.95
 
 config.tab_bar_at_bottom = true
 
 -- disable ligatures
-config.font = wezterm.font({
-	family = "JetBrains Mono",
-	weight = "Medium",
-	harfbuzz_features = { "calt=0", "clig=0", "liga=0" },
-})
+config.font = wezterm.font {
+  family = 'JetBrains Mono',
+  weight = 'Medium',
+  harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' },
+}
 
-config.leader = { key = "b", mods = "CTRL", timeout_milliseconds = 1000 }
+config.leader = { key = 'b', mods = 'CTRL', timeout_milliseconds = 1000 }
 
 config.window_padding = {
-	left = "0.5cell",
-	right = "0.5cell",
-	top = "0.5cell",
-	bottom = "0.5cell",
+  left = '0.5cell',
+  right = '0.5cell',
+  top = '0.5cell',
+  bottom = '0',
+}
+
+-- config.window_background_image = "/Users/sean/Documents/backgrounds/eva-3.jpg"
+
+config.window_background_image_hsb = {
+  -- Darken the background image by reducing it to 1/3rd
+  brightness = 0.3,
+
+  -- You can adjust the hue by scaling its value.
+  -- a multiplier of 1.0 leaves the value unchanged.
+  hue = 1.0,
+
+  -- You can adjust the saturation also.
+  saturation = 1.0,
 }
 
 local function is_vim(pane)
-	-- this is set by the plugin, and unset on ExitPre in Neovim
-	return pane:get_user_vars().IS_NVIM == "true"
+  -- this is set by the plugin, and unset on ExitPre in Neovim
+  return pane:get_user_vars().IS_NVIM == 'true'
 end
 
 local direction_keys = {
-	Left = "h",
-	Down = "j",
-	Up = "k",
-	Right = "l",
-	-- reverse lookup
-	h = "Left",
-	j = "Down",
-	k = "Up",
-	l = "Right",
+  Left = 'h',
+  Down = 'j',
+  Up = 'k',
+  Right = 'l',
+  -- reverse lookup
+  h = 'Left',
+  j = 'Down',
+  k = 'Up',
+  l = 'Right',
 }
 
 local function split_nav(resize_or_move, key)
-	return {
-		key = key,
-		mods = resize_or_move == "resize" and "META" or "CTRL",
-		action = wezterm.action_callback(function(win, pane)
-			if is_vim(pane) then
-				-- pass the keys through to vim/nvim
-				win:perform_action({
-					SendKey = { key = key, mods = resize_or_move == "resize" and "META" or "CTRL" },
-				}, pane)
-			else
-				if resize_or_move == "resize" then
-					win:perform_action({ AdjustPaneSize = { direction_keys[key], 3 } }, pane)
-				else
-					win:perform_action({ ActivatePaneDirection = direction_keys[key] }, pane)
-				end
-			end
-		end),
-	}
+  return {
+    key = key,
+    mods = resize_or_move == 'resize' and 'META' or 'CTRL',
+    action = wezterm.action_callback(function(win, pane)
+      if is_vim(pane) then
+        -- pass the keys through to vim/nvim
+        win:perform_action({
+          SendKey = { key = key, mods = resize_or_move == 'resize' and 'META' or 'CTRL' },
+        }, pane)
+      else
+        if resize_or_move == 'resize' then
+          win:perform_action({ AdjustPaneSize = { direction_keys[key], 3 } }, pane)
+        else
+          win:perform_action({ ActivatePaneDirection = direction_keys[key] }, pane)
+        end
+      end
+    end),
+  }
 end
 
 config.keys = {
-	{ key = "LeftArrow", mods = "CMD", action = act.ActivateTabRelative(-1) },
-	{ key = "RightArrow", mods = "CMD", action = act.ActivateTabRelative(1) },
-	-- Make Option-Left equivalent to Alt-b which many line editors interpret as backward-word
-	{ key = "LeftArrow", mods = "OPT", action = act.SendString("\x1bb") },
-	-- Make Option-Right equivalent to Alt-f; forward-word
-	{ key = "RightArrow", mods = "OPT", action = act.SendString("\x1bf") },
-	-- splitting
-	{
-		mods = "LEADER",
-		key = "-",
-		action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }),
-	},
-	{
-		mods = "LEADER",
-		key = "=",
-		action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }),
-	},
-	{
-		mods = "LEADER",
-		key = "m",
-		action = wezterm.action.TogglePaneZoomState,
-	},
-	-- rotate panes
-	{
-		mods = "LEADER",
-		key = "Space",
-		action = wezterm.action.RotatePanes("Clockwise"),
-	},
-	-- show the pane selection mode, but have it swap the active and selected panes
-	{
-		mods = "LEADER",
-		key = "0",
-		action = wezterm.action.PaneSelect({
-			mode = "SwapWithActive",
-		}),
-	},
-	{
-		key = "k",
-		mods = "CMD",
-		action = act.ClearScrollback("ScrollbackAndViewport"),
-	},
-	-- move between split panes
-	split_nav("move", "h"),
-	split_nav("move", "j"),
-	split_nav("move", "k"),
-	split_nav("move", "l"),
-	-- resize panes
-	split_nav("resize", "h"),
-	split_nav("resize", "j"),
-	split_nav("resize", "k"),
-	split_nav("resize", "l"),
-	{
-		key = "c",
-		mods = "CMD",
-		action = wezterm.action_callback(function(window, pane)
-			if pane:is_alt_screen_active() then
-				window:perform_action(wezterm.action.SendKey({ key = "y", mods = "CMD" }), pane)
-			else
-				window:perform_action(wezterm.action({ CopyTo = "ClipboardAndPrimarySelection" }), pane)
-			end
-		end),
-	},
+  { key = 'LeftArrow', mods = 'CMD', action = act.ActivateTabRelative(-1) },
+  { key = 'RightArrow', mods = 'CMD', action = act.ActivateTabRelative(1) },
+  -- Make Option-Left equivalent to Alt-b which many line editors interpret as backward-word
+  { key = 'LeftArrow', mods = 'OPT', action = act.SendString '\x1bb' },
+  -- Make Option-Right equivalent to Alt-f; forward-word
+  { key = 'RightArrow', mods = 'OPT', action = act.SendString '\x1bf' },
+  -- splitting
+  {
+    mods = 'LEADER',
+    key = '-',
+    action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' },
+  },
+  {
+    mods = 'LEADER',
+    key = '=',
+    action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' },
+  },
+  {
+    mods = 'LEADER',
+    key = 'm',
+    action = wezterm.action.TogglePaneZoomState,
+  },
+  -- rotate panes
+  {
+    mods = 'LEADER',
+    key = 'Space',
+    action = wezterm.action.RotatePanes 'Clockwise',
+  },
+  -- show the pane selection mode, but have it swap the active and selected panes
+  {
+    mods = 'LEADER',
+    key = '0',
+    action = wezterm.action.PaneSelect {
+      mode = 'SwapWithActive',
+    },
+  },
+  {
+    key = 'k',
+    mods = 'CMD',
+    action = act.ClearScrollback 'ScrollbackAndViewport',
+  },
+  -- move between split panes
+  split_nav('move', 'h'),
+  split_nav('move', 'j'),
+  split_nav('move', 'k'),
+  split_nav('move', 'l'),
+  -- resize panes
+  split_nav('resize', 'h'),
+  split_nav('resize', 'j'),
+  split_nav('resize', 'k'),
+  split_nav('resize', 'l'),
+  {
+    key = 'c',
+    mods = 'CMD',
+    action = wezterm.action_callback(function(window, pane)
+      if pane:is_alt_screen_active() then
+        window:perform_action(wezterm.action.SendKey { key = 'y', mods = 'CMD' }, pane)
+      else
+        window:perform_action(wezterm.action { CopyTo = 'ClipboardAndPrimarySelection' }, pane)
+      end
+    end),
+  },
 }
 
 for i = 1, 8 do
-	-- CTRL+ALT + number to move to that position
-	table.insert(config.keys, {
-		key = tostring(i),
-		mods = "CTRL|ALT",
-		action = wezterm.action.MoveTab(i - 1),
-	})
+  -- CTRL+ALT + number to move to that position
+  table.insert(config.keys, {
+    key = tostring(i),
+    mods = 'CTRL|ALT',
+    action = wezterm.action.MoveTab(i - 1),
+  })
 end
 
 -- and finally, return the configuration to wezterm
